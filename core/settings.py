55cbed3f1a1555ac13be16c93b14877f2e7d208c
Kristian Roopnarine <kristian.roopnarine@gmail.com>
2020-07-22 15:43:58 -0400

    first commit

--- core/settings.py
+++ core/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
-import mimetypes
-mimetypes.add_type("text/html", ".css", True)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
